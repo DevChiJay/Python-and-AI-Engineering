680e56c9eef126d8684d062a48a200ebb756f653
Dev Chi <littlejamesc@gmail.com>
2025-10-09 13:04:55 +0100

    learned tokenization

--- 03-hugging-face/main.py
+++ 03-hugging-face/main.py
@@ -1,13 +1,22 @@
 # Use a pipeline as a high-level helper
-from transformers import pipeline, infer_device
+from transformers import pipeline, infer_device, AutoTokenizer, AutoModelForCausalLM, GPT2Tokenizer, GPT2Model
 
-device = infer_device()
+checkpoint = "gpt2"
+tokenizer = GPT2Tokenizer.from_pretrained(checkpoint)
+model = GPT2Model.from_pretrained(checkpoint)
+text = "Grief has a way of "
 
-pipe = pipeline("text-generation", model="openai-community/gpt2", device=device)
+encoded_input = tokenizer(text, return_tensors="pt")
+output = model(**encoded_input)
 
-# Ensure a pad token is set (GPT-2 has no pad token by default)
-pipe.model.config.pad_token_id = pipe.model.config.eos_token_id
+print(output)
+# device = infer_device()
 
-out = pipe("The secret to acquiring wealth is ", max_new_tokens=50, truncation=True)
+# pipe = pipeline("text-generation", model="openai-community/gpt2", device=device)
 
-print(out)
+# # Ensure a pad token is set (GPT-2 has no pad token by default)
+# pipe.model.config.pad_token_id = pipe.model.config.eos_token_id
+
+# out = pipe("The secret to acquiring wealth is ", max_new_tokens=50, truncation=True)
+
+# print(out)
